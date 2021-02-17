713b1c56bc8a58553854f06cda8975eb8427a848
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2021-02-17 20:09:51 -0300

    Salvando dados enviado via formulario e enviando via api | exibindo os dados em ma tabela

--- app/controllers/carros_controller.rb
+++ app/controllers/carros_controller.rb
@@ -1,5 +1,6 @@
 class CarrosController < ApplicationController
   before_action :set_carro, only: %i[ show edit update destroy ]
+  skip_before_action :verify_authenticity_token
 
   # GET /carros or /carros.json
   def index
