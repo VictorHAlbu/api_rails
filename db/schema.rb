cf48192d58a9ef4e11768e9f5e980afdfd49ca94
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2021-02-24 10:19:49 -0300

    Token api segurança com gerenciador de token

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_09_133507) do
+ActiveRecord::Schema.define(version: 2021_02_24_131219) do
 
   create_table "carros", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "nome"
     t.string "ano"
     t.string "modelo"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tokens", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "valor"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
